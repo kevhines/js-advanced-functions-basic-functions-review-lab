766211626f2f2b11d2a4ff29e86a1dd32c04081d
Kevin <kevhines@gmail.com>
2021-04-23 11:04:30 -0400

    commit tree c63b16efe750ecc02db119d9f8d04b7c718b9017

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(fun = "roller-skate") {
+    return "This Saturday, I want to " + fun + "!"
+}
+
+function mondayWork(work = "go to the office") {
+    return "This Monday, I will " + work + "."
+}
+
+function wrapAdjective(flair = "*") {
+    return function(innervar = "special") {
+        return "You are " + flair + innervar + flair + "!"
+    }
+}
+
+let Calculator = {
+    add: function add(num1,num2) {return num1 + num2},
+    subtract: function subtract(num1,num2) {return num1 - num2},
+    multiply: function multiply(num1,num2) {return num1 * num2},
+    divide: function divide(num1,num2) {return num1 / num2}
+}
+
+function actionApplyer(start, funcArrays) {
+    let solution = start
+    if (funcArrays.length > 0) {
+        funcArrays.forEach(element => {
+            solution = element(solution)
+        })
+    }
+    return solution
+}
+
